430c0e2a53e57b7621d2316bf0163ec4d0a3b70e
EGC-Enrique <enruiq@gmail.com>
2020-11-29 02:07:33 +0100

    Añadido django para despliegue en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,17 +74,7 @@ MODULES = [
 #BASEURL = 'https://decide-septiembre2.herokuapp.com/'
 BASEURL = 'https://localhost:8000'
 
-APIS = {
-    'authentication': BASEURL,
-    'base': BASEURL,
-    'booth': BASEURL,
-    'census': BASEURL,
-    'mixnet': BASEURL,
-    'postproc': BASEURL,
-    'store': BASEURL,
-    'visualizer': BASEURL,
-    'voting': BASEURL,
-}
+APIS = {}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -193,3 +184,5 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+django_heroku.settings(locals()) 
