f242810c2c8c7a92244f962fd2629089be1a4fdf
EGC-Enrique <enruiq@gmail.com>
2020-12-01 17:52:34 +0100

    cambiado puerto despliegue

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku 
+#import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -71,8 +71,8 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'https://decide-septiembre3.herokuapp.com'
-#BASEURL = 'https://localhost:8000'
+#BASEURL = 'https://decide-septiembre3.herokuapp.com'
+BASEURL = 'https://localhost:8002'
 
 APIS = {
     'authentication': BASEURL,
@@ -195,4 +195,4 @@ if os.path.exists("config.jsonnet"):
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
 
-django_heroku.settings(locals()) 
+#django_heroku.settings(locals()) 
